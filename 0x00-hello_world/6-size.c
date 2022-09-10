e7d03349c84288dd6349e0bccb5cb0066c5e9a42
MO ATIA <eng.muhammad-com@hotmail.com>
2022-09-10 06:42:07 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@
 
 int main(void)
 {
-printf ("Size of a  %zu\n byte(s)", sizeof(char));
-printf ("Size of an %zu\n", sizeof(int));
-printf ("Size of a  %zu\n", sizeof(long int));
-printf ("Size of a  %zu\n", sizeof(long long int));
-printf ("Size of a  %zu\n", sizeof(float));
+printf ("Size of a  cahr: %zu byte(s)\n", sizeof(char));
+printf ("Size of an int: %zu byte(s)\n", sizeof(int));
+printf ("Size of a  long: int  %zu byte(s)\n", sizeof(long int));
+printf ("Size of a  long long int: %zu byte(s)\n", sizeof(long long int));
+printf ("Size of a  float: %zu byte(s)\n", sizeof(float));
 
 return (0);
 }
